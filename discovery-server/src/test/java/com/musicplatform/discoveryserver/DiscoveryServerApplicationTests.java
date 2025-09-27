970787080cf37406e0070463b112dcb166aeca59
NeskoraA <p.nedopekin@bk.ru>
2025-09-27 21:52:51 +0300

    Initial commit: Music Platform with Eureka, Config Server, API Gateway

---
+++ discovery-server/src/test/java/com/musicplatform/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicplatform.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
